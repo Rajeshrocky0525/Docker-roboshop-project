20b6b98da037993a1bcc0a17d7934ef9307ba1f8
rajesh <mrajesh0525@gmail.com>
2025-08-02 16:52:59 +0530

    docker-roboshop

---
+++ dockervolume.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-buildx-plugin -y
+systemctl enable docker
+systemctl start docker
+usermod -aG docker ec2-user
+
+growpart /dev/nvme0n1 4
+lvextend -L +20G /dev/RootVG/rootVol
+lvextend -L +10G /dev/RootVG/varVol
+
+xfs_growfs /
+xfs_growfs /var
